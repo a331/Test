403b57ad1c50cca8b477d124ab20200ad525f85e
a331 <tb975@ymail.com>
2016-07-20 15:56:45 +0800

    test

---
+++ src/AddTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class AddTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		assertEquals(12, new Add().add(3, 9));
+		
+	}
+
+}
